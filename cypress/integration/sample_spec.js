addd29b43a1662ae4e462b344eb9706179a72424
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-12 00:28:08 +0900

    click and url change

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,8 @@ describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('https://example.cypress.io')
 
-    cy.contains('type')
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
   })
 })
