f147054e2f5e272b9370f987c07a19dfa8819726
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-04-12 00:42:08 +0900

    test my local server

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,14 +1,5 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('https://example.cypress.io')
-
-    cy.contains('type').click()
-
-    cy.url().should('include', '/commands/actions')
-
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('.action-email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3001') // change URL to match your dev URL
   })
 })
