f824034d6554909a4bb569c1edbab400bb526051
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-07 14:53:23 +0900

    login test

--- cypress/integration/examples/home_page_spec.js
+++ cypress/integration/examples/home_page_spec.js
@@ -16,14 +16,25 @@ describe("The Home Page", () => {
       .as("currentUser");
   });
 
-  it("successfully loads", function () {
-    // this.currentUser will now point to the response
-    // body of the cy.request() that we could use
-    // to log in or work with in some way
+  it('sets auth cookie when logging in via form submission', function () {
+    // destructuring assignment of the this.currentUser object
+    const { username, password } = this.currentUser
 
-    cy.log("this");
-    cy.log(this);
-    cy.log("this.currentUser = ", this.currentUser);
-    cy.visit("/");
-  });
+    cy.visit('/login')
+
+    cy.get('input[name=username]').type(username)
+
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`${password}{enter}`)
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/dashboard')
+
+    // our auth cookie should be present
+    cy.getCookie('your-session-cookie').should('exist')
+
+    // UI should reflect this user being logged in
+    cy.get('h1').should('contain', 'jane.lane')
+  })
+})
 });
