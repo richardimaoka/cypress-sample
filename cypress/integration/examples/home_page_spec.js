fd79d21852367fc9eb3551c3cd1a7137b5774407
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-07 14:42:00 +0900

    https://github.com/cypress-io/cypress/issues/4880

--- cypress/integration/examples/home_page_spec.js
+++ cypress/integration/examples/home_page_spec.js
@@ -16,11 +16,14 @@ describe("The Home Page", () => {
       .as("currentUser");
   });
 
-  it("successfully loads", () => {
+  it("successfully loads", function () {
     // this.currentUser will now point to the response
     // body of the cy.request() that we could use
     // to log in or work with in some way
 
+    cy.log("this");
+    cy.log(this);
+    cy.log("this.currentUser = ", this.currentUser);
     cy.visit("/");
   });
 });
