3b0cd31c70b5cce03ba4da2039e7372907a0f5ca
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-05-07 13:10:17 +0900

    db run and post

--- cypress/integration/examples/home_page_spec.js
+++ cypress/integration/examples/home_page_spec.js
@@ -1,4 +1,12 @@
 describe("The Home Page", () => {
+  beforeEach(() => {
+    cy.exec("npm run db:reset && npm run db:seed");
+  });
+  cy.request("POST", "/test/seed/post", {
+    title: "First Post",
+    authorId: 1,
+    body: "...",
+  });
   it("successfully loads", () => {
     cy.visit("/"); // change URL to match your dev URL
   });
